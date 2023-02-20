eb83bf96b2419ab71685d7283fea31cab10826c8
Insu Hong <hinsu@rmsoft.kr>
2023-02-20 10:58:50 +0900

    aws key 설정

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-}
+  compiler: {
+    styledComponents: true,
+  },
+  env: {
+    ACCESS_KEY: process.env.AWS_ACCESSKEY,
+    SECRET_KEY: process.env.AWS_SECRET_ACCESSKEY,
+    RECOIL_DUPLICATE_ATOM_KEY_CHECKING_ENABLED: false,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
