56a1ad33f6b015aaef5e9252231a165033e3ff93
Insu Hong <hinsu@rmsoft.kr>
2023-02-20 15:21:30 +0900

    게시판 리스트 구현

--- pages/board/index.tsx
+++ pages/board/index.tsx
@@ -1,13 +1,40 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+
+const fetchList = async () => {
+  const response = await fetch("/api/list");
+  const json = await response.json();
+  return json;
+};
 
 const BoardList = () => {
+  const [list, setList] = useState([]);
+  const router = useRouter();
+  useEffect(() => {
+    fetchList().then((res) => setList(() => res.postList));
+  }, []);
+
   return (
     <div>
       <h1>글 목록</h1>
       <div>
         <Link href="/board/register">등록하기</Link>
       </div>
-
+      {list.length > 0 ? (
+        <div>
+          {list.map((item) => (
+            <div
+              key={item.postIdx}
+              onClick={() => router.replace(`/board/${item.postIdx}`)}
+            >
+              <div>{item.postTitle}</div>
+            </div>
+          ))}
+        </div>
+      ) : (
+        <div>Loading...</div>
+      )}
       <div>
         <Link href="/board/1">1번글</Link>
       </div>
