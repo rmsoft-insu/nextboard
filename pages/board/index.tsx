813c43508d9b995f172a576141c5dc2428d1e866
Insu Hong <hinsu@rmsoft.kr>
2023-02-24 14:01:50 +0900

    Update list checkbox

--- pages/board/index.tsx
+++ pages/board/index.tsx
@@ -9,11 +9,28 @@ const fetchList = async () => {
 };
 
 const BoardList = () => {
+  const [isCheckAll, setIsCheckAll] = useState(false);
+  const [isCheck, setIsCheck] = useState([]);
   const [list, setList] = useState([]);
+
   const router = useRouter();
   useEffect(() => {
     fetchList().then((res) => setList(() => res.postList));
   }, []);
+
+  const handleSelectAll = () => {
+    setIsCheckAll(!isCheckAll);
+    setIsCheck(list.map((item) => `${item.postIdx}`));
+    isCheckAll && setIsCheck([]);
+  };
+
+  const handleClick = (event) => {
+    const { id, checked } = event.target;
+    setIsCheck([...isCheck, id]);
+    if (!checked) {
+      setIsCheck(isCheck.filter((item) => item !== id));
+    }
+  };
 
   return (
     <div>
@@ -23,12 +40,29 @@ const BoardList = () => {
       </div>
       {list.length > 0 ? (
         <div>
+          <label htmlFor="selectAll">
+            <input
+              type="checkbox"
+              name="selectAll"
+              id="selectAll"
+              onChange={handleSelectAll}
+              checked={isCheckAll}
+            />
+            전체 선택
+          </label>
           {list.map((item) => (
-            <div
-              key={item.postIdx}
-              onClick={() => router.replace(`/board/${item.postIdx}`)}
-            >
-              <div>{item.postTitle}</div>
+            <div key={item.postIdx}>
+              <div>
+                <input
+                  type="checkbox"
+                  id={item.postIdx}
+                  onChange={handleClick}
+                  checked={isCheck.includes(`${item.postIdx}`)}
+                />
+                <span onClick={() => router.replace(`/board/${item.postIdx}`)}>
+                  {item.postTitle}
+                </span>
+              </div>
             </div>
           ))}
         </div>
