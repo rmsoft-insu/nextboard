6bb606e6cf929aea52466964498876ef095480ac
Insu Hong <hinsu@rmsoft.kr>
2023-03-06 17:23:40 +0900

    게시글 리스트 페이지

---
+++ pages/api/category/index.ts
@@ -0,0 +1,14 @@
+import client from "@/util/client";
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  try {
+    const data = await client.category.findMany();
+    res.status(200).send({ data: data });
+  } catch (error) {
+    res.status(400).send({ error: "Error" });
+  }
+}
--- pages/category/index.tsx
+++ pages/category/index.tsx
@@ -1,14 +1,69 @@
 import Link from "next/link";
+import { useEffect, useState } from "react";
+
+const categoryList = [
+  { idx: 1, category: "영화", code: "movie" },
+  { idx: 2, category: "도서", code: "book" },
+];
+
+const movieKind = [
+  { idx: 1, kind: "액션", code: "action" },
+  { idx: 2, kind: "로맨스", code: "romance" },
+  { idx: 3, kind: "드라마", code: "drama" },
+  { idx: 4, kind: "공포", code: "horror" },
+];
+
+const bookKind = [
+  { idx: 1, kind: "소설", code: "novel" },
+  { idx: 2, kind: "과학", code: "science" },
+  { idx: 3, kind: "기술/공학", code: "tech" },
+];
+
+const fetchList = async () => {
+  const response = await fetch("/api/category");
+  const json = response.json();
+  return json;
+};
+
+const SortBox = () => {
+  const [category, setCategory] = useState("");
+
+  return (
+    <div>
+      <h1>SortBox</h1>
+      {categoryList.map((item) => (
+        <div key={item.idx} onClick={() => console.log(item)}>
+          <div>{item.category}</div>
+        </div>
+      ))}
+    </div>
+  );
+};
 
 const Category = () => {
+  const [list, setList] = useState([]);
+
+  useEffect(() => {
+    fetchList().then((res) => {
+      console.log(res.data);
+      setList(() => res.data);
+    });
+  }, []);
+
   return (
     <div>
       <h1> 카테고리 게시판</h1>
+      <SortBox />
       <div>
         <div>
           <Link href="/category/register">카테고리 등록</Link>
         </div>
         <div>Category</div>
+        {list.map((item) => (
+          <div key={item.idx}>
+            <div>{item.title}</div>
+          </div>
+        ))}
       </div>
     </div>
   );
