57b23f0426133f20ff92a4785b652020bff109e8
Insu Hong <hinsu@rmsoft.kr>
2023-03-07 16:01:18 +0900

    dummy 제거

--- pages/category/register.tsx
+++ pages/category/register.tsx
@@ -1,19 +1,6 @@
 import { useEffect, useRef, useState } from "react";
 import { useForm } from "react-hook-form";
 
-const movieKind = [
-  { idx: 1, kind: "액션", code: "action" },
-  { idx: 2, kind: "로맨스", code: "romance" },
-  { idx: 3, kind: "드라마", code: "drama" },
-  { idx: 4, kind: "공포", code: "horror" },
-];
-
-const bookKind = [
-  { idx: 1, kind: "소설", code: "novel" },
-  { idx: 2, kind: "과학", code: "science" },
-  { idx: 3, kind: "기술/공학", code: "tech" },
-];
-
 const fetchPost = async (data) => {
   const response = await fetch("/api/category/register", {
     method: "POST",
