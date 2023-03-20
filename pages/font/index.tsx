6ee36a1a4872d832f59cc85c7244b54d6e95537c
Insu Hong <hinsu@rmsoft.kr>
2023-03-20 11:03:16 +0900

    preload 안되는 폰트 적용

--- pages/font/index.tsx
+++ pages/font/index.tsx
@@ -1,11 +1,42 @@
-import { Noto_Sans_KR } from "@next/font/google";
+import {
+  Noto_Sans_KR,
+  Edu_NSW_ACT_Foundation,
+  Noto_Sans_JP,
+  Rampart_One,
+  Noto_Sans_TC,
+  ZCOOL_QingKe_HuangYou,
+} from "@next/font/google";
 import styled from "styled-components";
 import localFont from "@next/font/local";
 
-const notoSansKR = Noto_Sans_KR({
+const notoSansKR = Edu_NSW_ACT_Foundation({
   weight: "700",
   style: ["normal"],
   subsets: ["latin"],
+});
+
+const notoSansJP = Rampart_One({
+  weight: "400",
+  style: ["normal"],
+  preload: false,
+});
+
+const notoSansJP2 = Noto_Sans_JP({
+  weight: "400",
+  style: ["normal"],
+  preload: false,
+});
+
+const notoTC = Noto_Sans_TC({
+  weight: "400",
+  style: ["normal"],
+  preload: false,
+});
+
+const zcool = ZCOOL_QingKe_HuangYou({
+  weight: "400",
+  style: ["normal"],
+  preload: false,
 });
 
 /* const pretendard = localFont({
@@ -21,7 +52,9 @@ const TextBox = styled.p`
   font-weight: ${notoSansKR.style.fontWeight};
 `;
 
-const TextBox2 = styled.p``;
+const TextBox2 = styled.p`
+  font-family: ${notoSansJP.style.fontFamily};
+`;
 
 const FontTest = () => {
   return (
@@ -34,11 +67,17 @@ const FontTest = () => {
         aspernatur rerum quos laboriosam!
       </TextBox>
       <TextBox2>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aliquam
-        fugiat, repellat corporis et incidunt quia, voluptatum nostrum veritatis
-        consectetur, iure repudiandae. Fugiat laborum cum exercitationem, ut
-        aspernatur rerum quos laboriosam!
+        人類社会のすべての構成員の固有の尊厳と平等で譲ることのできない権利とを承認することは
       </TextBox2>
+      <p className={notoSansJP2.className}>
+        人類社会のすべての構成員の固有の尊厳と平等で譲ることのできない権利とを承認することは
+      </p>
+      <p className={notoTC.className}>
+        鑑於對人類家庭所有成員的固有尊嚴及其平等的和不移的權利的承認，乃是世界自由、正義與和平的基礎
+      </p>
+      <p className={zcool.className}>
+        鉴于对人权的无视和侮蔑已发展为野蛮暴行,这些暴行玷污了人类的良心,而一个人人享有言论和信仰自由并免予恐惧和匮乏的世界的来临,已被宣布为普通人民的最高愿望
+      </p>
     </div>
   );
 };
