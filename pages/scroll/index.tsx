5b1271eeed6d3327a540b7da97b974211c53ef81
iinsue <hinsu@rmsoft.kr>
2023-03-26 22:01:59 +0900

    css 입히기

--- pages/scroll/index.tsx
+++ pages/scroll/index.tsx
@@ -1,4 +1,16 @@
 import { useEffect, useState } from "react";
+import styled from "styled-components";
+
+const Box = styled.div`
+  background-color: burlywood;
+  width: 100%;
+  height: 200px;
+  margin-bottom: 1rem;
+  span {
+    margin-right: 10px;
+    font-weight: bold;
+  }
+`;
 
 const fetchList = async () => {
   const response = await fetch(`http://localhost:4000/todos`);
@@ -17,10 +29,10 @@ const InfiniteScroll = () => {
     <>
       <h1>한무 스크롤</h1>
       {list?.map((item, index) => (
-        <div key={item.id}>
+        <Box key={item.id}>
           <span>No.{index + 1}</span>
           <span>{item.description}</span>
-        </div>
+        </Box>
       ))}
     </>
   );
