565a6ffa3f364a83893af447f085bb9c89216d56
iinsue <118152974+iinsu@users.noreply.github.com>
2023-04-10 22:01:47 +0900

    animation

--- pages/motion/index.tsx
+++ pages/motion/index.tsx
@@ -1,11 +1,13 @@
 import { Variants, motion } from "framer-motion";
 import { useState } from "react";
+import { useFieldArray, useForm } from "react-hook-form";
 import styled from "styled-components";
 
 const itemVariants: Variants = {
   open: {
     transformOrigin: "top",
     scaleY: 1,
+    height: "80px",
     transition: { duration: 0.2 },
   },
   closed: {
@@ -33,8 +35,41 @@ const Btn1 = styled.div`
   cursor: pointer;
 `;
 
-const Motion = () => {
+const Box2 = styled.div`
+  width: 100px;
+  height: 100px;
+  background-color: pink;
+`;
+const Box3 = styled(motion.div)`
+  width: 80px;
+  height: 80px;
+  background-color: skyblue;
+`;
+
+const ArrayItem = (props) => {
+  const { index } = props;
   const [isOpen, setIsOpen] = useState(false);
+  console.log(isOpen);
+  return (
+    <Box2>
+      <div onClick={() => setIsOpen((prev) => !prev)}>클릭</div>
+      <Box3
+        initial={false}
+        animate={isOpen ? "open" : "closed"}
+        variants={itemVariants}
+      ></Box3>
+      <div>123</div>
+    </Box2>
+  );
+};
+
+const Motion = () => {
+  const { register, control, handleSubmit } = useForm({});
+  const { fields, append, remove } = useFieldArray({ control, name: "test" });
+  const [isOpen, setIsOpen] = useState(true);
+
+  const submitEvent = (data) => console.log(data);
+
   return (
     <div>
       <h1>Motion</h1>
@@ -47,7 +82,15 @@ const Motion = () => {
       >
         <Box1></Box1>
       </motion.div>
-      <Box1></Box1>
+      <form onSubmit={handleSubmit(submitEvent)}>
+        {fields.map((item, index) => (
+          <div key={item.id}>
+            <ArrayItem index={index} />
+            <input {...register(`test.${index}.firstName`)} />
+          </div>
+        ))}
+      </form>
+      <button onClick={() => append({})}>Append</button>
     </div>
   );
 };
