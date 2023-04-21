cc071d6eb059cc904cdd9b737aaebca727014a6a
Insu Hong <hinsu@rmsoft.kr>
2023-04-21 09:40:58 +0900

    Component 에 Style 적용

--- pages/examples/styleprop.tsx
+++ pages/examples/styleprop.tsx
@@ -1,10 +1,18 @@
 import ExampleComponent from "@/components/examples/PropExample";
+import styled from "styled-components";
+
+const StyleWrapper = styled(ExampleComponent)`
+  width: 300px;
+  height: 300px;
+  background-color: burlywood;
+  color: blue;
+`;
 
 const StyleProp = () => {
   return (
-    <ExampleComponent>
+    <StyleWrapper>
       <div>예제 컨텐츠 입니다.</div>
-    </ExampleComponent>
+    </StyleWrapper>
   );
 };
 
