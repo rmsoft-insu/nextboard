39b132a356926e5d65e269efffe03c492154a367
Insu Hong <118152974+iinsu@users.noreply.github.com>
2023-07-26 10:44:19 +0900

    tooltip 설정

--- pages/tip/index.tsx
+++ pages/tip/index.tsx
@@ -1,8 +1,36 @@
+import { Tooltip } from "react-tooltip";
+import styled from "styled-components";
+import { MdOutlineInfo } from "react-icons/md";
+
+const InfoLabel = styled(Tooltip)`
+  background-color: #3e444e;
+  color: #ffffff;
+`;
+
+const InfoIconWrapper = styled.span`
+  background-color: #000000;
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  color: #d8d8d8;
+  :hover {
+    color: #ffffff;
+  }
+`;
+
 const ToolTipPage = () => {
   return (
-    <div>
-      <div></div>
-    </div>
+    <>
+      <div>
+        <InfoIconWrapper data-tooltip-id="app">
+          <MdOutlineInfo size={24} />
+        </InfoIconWrapper>
+      </div>
+      <InfoLabel id="app" place="bottom">
+        <div>Hello World!</div>
+        <div>Hi World</div>
+      </InfoLabel>
+    </>
   );
 };
 
